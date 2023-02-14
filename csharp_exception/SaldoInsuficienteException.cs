f4dc467a91736cc2fce7a6ae65c0811ed78165c7
Larissa Gabriela <66263337+larissagabriela@users.noreply.github.com>
2023-02-14 16:56:57 -0300

    Add files via upload

---
+++ csharp_exception/SaldoInsuficienteException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exception
+{
+
+    public class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException(string mensagem) : base(mensagem)
+        {
+
+        }
+
+    }
+}
